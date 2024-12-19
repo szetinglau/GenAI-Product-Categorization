41c3387e24970285bc28a7be49672ecf30a8d2e1
Kathy <146206163+szetinglau@users.noreply.github.com>
2024-12-19 12:40:26 -0500

    Update and rename pfg_product_categorization.py to product_categorization.py

--- product_categorization.py
+++ product_categorization.py
@@ -1,3 +1,11 @@
+'''
+TODO Turn off content filter for this. It triggers maybe once every 100 product items when classifying through gpt-4o: Content filter error: Could not parse response content as the request was rejected by the content filter
+TODO Need to spit out each search results + prompt going to GPT-4o for verification purposes
+TODO Update Readme as this might be a highly repeatable solution
+TODO Deployable solution idea: Web UI, drop in a file in this format, columns, rows, each even row hasn't been looked at, each odd row is ground truth. Spits out the accuracy of having Azure AI Search + GPT-4o/4o-mini/o1 make the product categorization
+TODO Make a One Click Deploy, yes it'll be painful, but do it and you enhance repeatability
+TODO Test with Blanks on Class
+'''
 
 import openai
 import pandas as pd
